ff4ea020d30a38b32a03250d49d7b60e92cafc91
guoyongjian <yongjian.guo@houkeyun.com>
2018-04-09 18:35:22 +0800

    注册页面

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,10 +1,92 @@
 <template>
-    <div>Hello Login!</div>
+  <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+    <el-form-item label="用户名" prop="userName">
+      <el-input v-model.number="ruleForm2.userName"></el-input>
+    </el-form-item>
+    <el-form-item label="密码" prop="pass">
+      <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="确认密码" prop="checkPass">
+      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+      <el-button @click="resetForm('ruleForm2')">重置</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script type="text/ecmascript-6">
   export default {
-    name: 'Login'
+    name: 'Login',
+    data() {
+      var validateUserName = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入用户名'))
+        } else {
+          let userNameReg = /^[a-z]\w{3,15}/
+          if (!userNameReg.exec(value)) {
+            callback(new Error('用户名为4-16个字母或数字,且开头必须为字母'))
+          } else {
+            callback()
+          }
+        }
+      }
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'))
+        } else {
+          if (this.ruleForm2.checkPass !== '') {
+            this.$refs.ruleForm2.validateField('checkPass')
+          }
+          callback()
+        }
+      }
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'))
+        } else if (value !== this.ruleForm2.pass) {
+          callback(new Error('两次输入密码不一致!'))
+        } else {
+          callback()
+        }
+      }
+      return {
+        ruleForm2: {
+          userName: '',
+          pass: '',
+          checkPass: ''
+        },
+        rules2: {
+          userName: [
+            {
+              validator: validateUserName, required: true, trigger: 'blur'
+            }
+          ],
+          pass: [
+            {validator: validatePass, required: true, trigger: 'blur'}
+          ],
+          checkPass: [
+            {validator: validatePass2, required: true, trigger: 'blur'}
+          ]
+        }
+      }
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!')
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields()
+      }
+    }
   }
 </script>
 
