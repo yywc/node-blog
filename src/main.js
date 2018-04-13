12509aa068f533631a4b5913eaaf4bebfc43477e
gyjyywc <gyjyywc@gmail.com>
2018-04-13 18:01:06 +0800

    登录验证的前期准备

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import router from './router'
 import fastclick from 'fastclick'
 import VueLazyLoad from 'vue-lazyload'
 import ElementUI from 'element-ui'
+// import axios from 'axios'
+// import NProgress from 'nprogress'
+// import store from 'store'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/stylus/index.styl'
 
@@ -13,6 +16,80 @@ Vue.config.productionTip = false
 
 fastclick.attach(document.body)
 Vue.use(ElementUI)
+
+// NProgress.configure({
+//   showSpinner: false,
+//   speed: 150,
+//   minimum: 0.2,
+//   trickleRate: 0.05,
+//   trickleSpeed: 500,
+//   easing: 'ease'
+//   // speed: 500
+// })
+//
+// router.beforeEach((to, from, next) => {
+//
+//   NProgress.start()
+//
+//   // 已登录判断
+//   if (to.path === '/login') {
+//     if (store.get('__token__') == null) {
+//       next()
+//     } else {
+//       next({
+//         path: '/home'
+//       })
+//     }
+//     NProgress.done()
+//     return
+//   }
+//
+//   if (store.get('__token__') != null) {
+//     next()
+//   } else {
+//     console.log('not login.')
+//     next({
+//       path: '/login',
+//       query: {redirect: to.fullPath}
+//     })
+//   }
+// })
+//
+// router.afterEach(transition => {
+//   console.log('stop progress')
+//   NProgress.done()
+// })
+//
+// axios.interceptors.request.use(function (config) {
+//   NProgress.start()
+//   return config
+// }, function (error) {
+//   NProgress.done()
+//   return Promise.reject(error)
+// })
+//
+// axios.interceptors.request.use(function (config) {
+//   if (store.get('__token__') != null) {
+//     config.headers['x-auth-token'] = store.get('__token__')
+//   }
+//   return config
+// }, function (err) {
+//   return Promise.reject(err)
+// })
+//
+// axios.interceptors.response.use(function (response) {
+//   NProgress.done()
+//   // 登出
+//   console.log('AXIOS INTERC' + response.data.status)
+//   if (response.data.status === 110) {
+//     store.clearAll()
+//     router.push('/login')
+//   }
+//   return response
+// }, function (error) {
+//   NProgress.done()
+//   return Promise.reject(error)
+// })
 
 Vue.use(VueLazyLoad, {
   loading: require('@/assets/imgs/default.png'),
@@ -23,6 +100,6 @@ Vue.use(VueLazyLoad, {
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
