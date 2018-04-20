5581d17022d6183ecd253c9ef0bdcb7987af7a77
yywc <gyjyywc@gmail.com>
2018-04-20 20:15:18 +0800

    文章界面

--- src/components/article/article.vue
+++ src/components/article/article.vue
@@ -1,10 +1,22 @@
 <template>
-    <div></div>
+  <el-container>
+    <m-header></m-header>
+    <el-footer>
+      <m-footer></m-footer>
+    </el-footer>
+  </el-container>
 </template>
 
 <script type="text/ecmascript-6">
+  import MHeader from '@/common/m-header/m-header'
+  import MFooter from '@/common/m-footer/m-footer'
+
   export default {
-    name: 'Article'
+    name: 'Article',
+    components: {
+      MFooter,
+      MHeader
+    }
   }
 </script>
 
