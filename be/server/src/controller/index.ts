18f05c0f95c7e865ea338ddf6430ab1d217dd218
yywc <gyjyywc@gmail.com>
2019-07-11 18:13:04 +0800

    refactor(controller): 控制器使用 glob 加载文件

--- be/server/src/controller/index.ts
+++ be/server/src/controller/index.ts
@@ -1,57 +1,32 @@
-import * as fs from 'fs';
+import * as glob from 'glob';
 import * as path from 'path';
 
 export default class Controller {
   private static tree: Record<string, any> = {};
   private static isLoaded = false;
 
-  private static set(d = __dirname): Controller {
+  private static register(): Controller {
     const tree: Record<string, any> = {};
-    const dirs: string[] = []; // 文件夹数组
-    const files: string[] = []; // 文件数组
-
-    fs.readdirSync(d).forEach((item: string): void => {
-      const isDir = fs.statSync(path.join(d, item)).isDirectory();
-      if (isDir) {
-        dirs.push(item);
-      } else if (item !== 'index.ts') {
-        files.push(item);
+    const paths = glob.sync(`${__dirname}/**/*.ts`);
+    paths.forEach((p): void => {
+      const name = path.relative(__dirname, p);
+      // 不挂载当前文件
+      if (name !== 'index.ts' && path.extname(name) === '.ts') {
+        tree[name.replace('.ts', '')] = require(p).default;
       }
     });
-
-    // 映射文件夹
-    dirs.forEach((dir): void => {
-      tree[dir] = Controller.set(path.join(d, dir));
-    });
-
-    // 映射文件
-    files.forEach((file): void => {
-      if (path.extname(file) === '.ts') {
-        tree[path.basename(file, '.ts')] = require(path.join(d, file)).default;
-      }
-    });
-
-    Controller.isLoaded = true;
-
     return tree;
   }
 
   public static get<T>(key: string): T {
     if (!Controller.isLoaded) {
-      Controller.tree = Controller.set();
+      Controller.tree = Controller.register();
     }
-    const keys = key.split('/');
-    const getResult = (keys: string[]): any => {
-      const node = Controller.tree[keys[0]];
-      if (keys.length === 1) {
-        if (!node) {
-          throw new Error(`Cannot find model of '${key}', key muse be the same as class name`);
-        }
-        return node;
-      }
-      keys.shift();
-      return getResult(keys);
-    };
-    return getResult(keys);
+    const node = Controller.tree[key];
+    if (!node) {
+      throw new Error(`Cannot find model of '${key}', 
+      key muse be like <[controller path/]filename or directory_name/filename>`);
+    }
+    return node;
   }
 }
